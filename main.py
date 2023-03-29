093f5ad654d3b15fe8025be2630a3992769aaf96
WWunn <wojciech.widzgowski@gmail.com>
2023-03-29 16:44:50 +0200

    f

--- main.py
+++ main.py
@@ -14,15 +14,15 @@
 #     listc=open('listc','a+')
 #     for i in range(1,len(lista)):
 #         listc.write(lista[i]+listb[i])
-n=int(input())
-suma = 0
-for i in range(1, n+1):
-    suma=suma+i
-
-print(suma)
-zrycie=open('likkt','w+')
-zrycie.write(4)
-x=zrycie.read(1)
-y=zrycie.read(1)
+# n=int(input())
+# suma = 0
+# for i in range(1, n+1):
+#     suma=suma+i
+#
+# print(suma)
+# zrycie=open('likkt','w+')
+# zrycie.write(4)
+# x=zrycie.read(1)
+# y=zrycie.read(1)
 
 
